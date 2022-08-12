5ed243d5a696c0957a713576b71e2647d8639c43
LaureCHAPERT <laure.chapert@gmail.com>
2022-08-12 17:29:53 +0200

    We redirect when slug isn't known

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -42,6 +42,14 @@ class PropertyController extends AbstractController
   public function show($slug, $id): Response
   {
     $property = $this->repository->find($id);
+
+    //if slug doesn't exist we redirect on latest slug route
+    if ($property->getSlug() !== $slug) {
+      return $this->redirectToRoute('property.show', [
+        'id' => $property->getId(),
+        'slug' => $property->getSlug()
+      ], 301);
+    }
     return $this->render('property/show.html.twig', [
       'property' => $property,
       'current_menu' => 'properties'
