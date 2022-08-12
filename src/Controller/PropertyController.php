b7c29209e57232f190ffa520002d409ba62eb462
LaureCHAPERT <laure.chapert@gmail.com>
2022-08-12 16:19:44 +0200

    How to update an element with getManager and flushh from ManagerRegistry

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Property;
 use App\Repository\PropertyRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,9 +16,10 @@ class PropertyController extends AbstractController
    */
   private $repository;
 
-  public function __construct(PropertyRepository $repository)
+  public function __construct(PropertyRepository $repository, ManagerRegistry $doctrine)
   {
     $this->repository = $repository;
+    $this->doctrine = $doctrine;
   }
 
   /**
@@ -29,7 +31,11 @@ class PropertyController extends AbstractController
   {
     // Data recovery (Repository)
     $property = $this->repository->findAllVisible();
+    $property[1]->setSold(true);
+    $entityManager = $this->doctrine->getManager();
+    $entityManager->flush();
     dump($property);
+
     return $this->render('property/index.html.twig', [
       'current_menu' => 'properties'
     ]);
