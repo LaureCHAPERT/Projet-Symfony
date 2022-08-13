2cae4e7d7ec416ce1814e59a0b31e4a1a46a2213
LaureCHAPERT <laure.chapert@gmail.com>
2022-08-13 14:21:39 +0200

    Edited form is ok

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -8,7 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\PropertyType;
-
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminPropertyController extends AbstractController
 {
@@ -18,9 +19,10 @@ class AdminPropertyController extends AbstractController
    */
   private $repository;
 
-  public function __construct(PropertyRepository $repository)
+  public function __construct(PropertyRepository $repository, ManagerRegistry $doctrine)
   {
     $this->repository = $repository;
+    $this->doctrine = $doctrine;
   }
 
   /**
@@ -39,9 +41,16 @@ class AdminPropertyController extends AbstractController
    * @param Property $property
    * @return \Symfony\Component\HttpFoundation\Response;
    */
-  public function edit(Property $property)
+  public function edit(Property $property, Request $request)
   {
     $form = $this->createForm(PropertyType::class, $property);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager = $this->doctrine->getManager();
+      $entityManager->flush();
+      return $this->redirectToRoute('admin.property.index');
+    }
     return $this->render('admin/property/edit.html.twig', [
       'property' => $property,
       'form' => $form->createView()
